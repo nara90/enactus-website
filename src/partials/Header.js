a82723936d83cc43eefdf4bef951b90f1e7cba63
narayan kumar <narayankr629@gmail.com>
2021-12-17 22:06:51 +0530

    changes

---
+++ src/partials/Header.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+
+  const [top, setTop] = useState(true);
+
+  // detect whether user has scrolled the page down by 10px 
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true)
+    };
+    window.addEventListener('scroll', scrollHandler);
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, [top]);  
+
+  return (
+    <header className={`fixed w-full z-30 md:bg-opacity-60 transition duration-300 ease-in-out ${!top && 'bg-yellow-100 blur shadow-lg'}`}>
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+
+          {/* Site branding */}
+          <div className="flex-shrink-0 mr-4">
+            
+            {/* Logo */}
+            <Link to="" className="block" aria-label="Cruip">
+              <img className="w-10 h-10" src={require('../images/headerlogo.png').default} alt="logo"/>
+              {/*<svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                  <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="header-logo">
+                    <stop stopColor="#4FD1C5" offset="0%" />
+                    <stop stopColor="#81E6D9" offset="25.871%" />
+                    <stop stopColor="#338CF5" offset="100%" />
+                  </radialGradient>
+                </defs>
+                <rect width="32" height="32" rx="16" fill="url(#header-logo)" fillRule="nonzero" />
+              </svg>*/}
+              
+            </Link>
+          </div>
+
+          {/* Site navigation */}
+          <nav className="flex flex-grow">
+            <ul className="flex flex-grow justify-end flex-wrap items-center">
+            <li>
+                <Link to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                  <span>Home</span>
+                                    
+                </Link>
+              </li>
+            <li>
+                <Link to="/signin" className="font-medium text-gray-700 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Project M</Link>
+              </li>
+              <li>
+                <Link to="/signin" className="font-medium text-gray-700 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">project Sana</Link>
+              </li>
+              
+            </ul>
+
+          </nav>
+
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
